df2786780788c43c5f0d5cb947e05e5212872946
Ops <krzysztof.papciak@hotmail.com>
2021-04-21 12:59:59 +0200

    remove unused classes

--- src/Shared/SharedDomain/Interfaces/IDomainEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using SuperCar.Shared.Domain.Abstraction;
-
-namespace SuperCar.Shared.Domain.Interfaces
-{
-    public interface IDomainEventHandler<T> : INotificationHandler<DomainEventNotification<T>> where T : IDomainEvent
-    {
-    }
-}
--- src/Shared/SharedDomain/Interfaces/IEventRepository.cs
+++ src/Shared/SharedDomain/Interfaces/IEventRepository.cs
@@ -6,7 +6,7 @@ namespace SuperCar.Shared.Domain.Interfaces
 {
     public interface IEventRepository
     {
-        Task<T> LoadAggregate<T,TY>(Identity id, CancellationToken cancellationToken = default) where T : AggregateRoot<TY> where  TY: Identity;
+        Task<T> LoadAggregate<T,TIdenity>(Identity id, CancellationToken cancellationToken = default) where T : AggregateRoot<TIdenity> where TIdenity : Identity;
 
         Task Save<T>(Identity identity, AggregateRoot<T> aggregate, CancellationToken cancellationToken = default)
             where T : Identity;
